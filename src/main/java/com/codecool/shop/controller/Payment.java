fba66ae541322ab3a60f143e0daedf943ba69c17
akosVogel <vogelakos@gmail.com>
2018-08-01 14:44:43 +0200

    payment system ok

--- src/main/java/com/codecool/shop/controller/Payment.java
+++ src/main/java/com/codecool/shop/controller/Payment.java
@@ -10,11 +10,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/payment"})
 public class Payment extends HttpServlet {
+
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,12 +35,29 @@ public class Payment extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        Map<String,String[]> parameterMap = req.getParameterMap();
 
-        Map parameterMap = req.getParameterMap();
+        if (parameterMap.get("card-holder-name")[0].length()>0 && parameterMap.get("card-number")[0].length()==10
+                && parameterMap.get("cvv")[0].length()==3 && parameterMap.get("card-holder-name")[0].equals("xyz")
+                &&  parameterMap.get("card-number")[0].equals("0123456789") && parameterMap.get("cvv")[0].equals("123"))
+            {
+            final JFrame parent = new JFrame();
+            JButton button = new JButton();
+            button.setText("Succesfull paying");
+            parent.add(button);
+            parent.pack();
+            parent.setVisible(true);
+            resp.sendRedirect("/");
+
+        }
         System.out.println(parameterMap);
-        
-
         context.setVariable("cash", "9000000");
         engine.process("product/paypal.html", context, resp.getWriter());
+        final JFrame parent = new JFrame();
+        JButton button = new JButton();
+        button.setText("Wrong data");
+        parent.add(button);
+        parent.pack();
+        parent.setVisible(true);
     }
 }
