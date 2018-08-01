2e9b3804a1fdb4118ee89a53634310b8be858a0d
laszloOrha <orha.laszlo.93@gmail.com>
2018-08-01 10:04:07 +0200

    shopping cart details created, prices summarised according to quantities and items

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,11 +1,14 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartItems;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.OrderItem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -20,9 +23,14 @@ import java.io.IOException;
 public class CartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cartData = CartItems.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("cartItems", cartData.getAll());
+        context.setVariable("totalPrice", OrderItem.getTotalPrice());
+
+
 
         engine.process("cart.html", context, resp.getWriter());
     }
--- src/main/java/com/codecool/shop/model/OrderItem.java
+++ src/main/java/com/codecool/shop/model/OrderItem.java
@@ -1,16 +1,28 @@
 package com.codecool.shop.model;
 
+import java.util.Currency;
+
 public class OrderItem {
     private int id;
     private String name;
     private int quantity;
+    private float price;
+    private Currency currency;
     public static int totalItems = 0;
+    public static float totalPrice = 0;
 
-    public OrderItem(int id, String name, int quantity) {
+    public static String getTotalPrice() {
+        return "Total value: " + String.valueOf(totalPrice) + " kUSD";
+    }
+
+    public OrderItem(int id, String name, int quantity, float price, Currency currency) {
         this.id = id;
         this.quantity = quantity;
         this.name = name;
+        this.price = price;
+        this.currency = currency;
         totalItems ++;
+        totalPrice += price;
     }
 
     public String getName() {
@@ -25,8 +37,19 @@ public class OrderItem {
         return id;
     }
 
+    public float getPrice() {
+        return price * quantity;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
     public void increaseQuantity() {
         this.quantity ++;
         totalItems ++;
+    }
+    public void increaseTotalPrice() {
+        totalPrice += price;
     }
 }
